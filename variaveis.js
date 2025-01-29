6dd20da5cc9e5256ecf4fd9985bb26080db5dc4a
felipelinodev <felipelinodesigndoe@gmail.com>
2025-01-29 06:56:15 -0300

    explicando-variaveis-var

---
+++ variaveis.js
@@ -0,0 +1,6 @@
+/* var -> Essa é a forma mais antiga de usar variáveis no javascript, mas muito pouco usada hoje em dia, visto que esse variáveis declarada com (var) não tem escopo.
+   Vou demonstrar isso.
+*/
+
+console.log(nome)
+var nome = 'João'
