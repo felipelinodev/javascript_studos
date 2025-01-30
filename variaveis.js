fc7a65bb7e0891e2a85af4abc2c7d8d36adcce7c
felipelinodev <felipelinodesigndoe@gmail.com>
2025-01-30 06:53:01 -0300

    explicando-variaveis-let

--- variaveis.js
+++ variaveis.js
@@ -3,8 +3,12 @@
 */
 
 console.log(nome) // Undefined, fignifica que a variável foi declara lá no topo do código. Aqui podemos notar que o javascript não estár sefuindo a ordem, ou seja ela foi declarada mas não definida.
-var nome = 'João' //Variáveis com var, tem escopo global ou de função,se não for em uma função tem escopo global, se for tem escopo apenas de função, isso pode gerar diversos problemas.
+var nome = 'João' //Variáveis com (var), tem escopo global ou de função,se não for em uma função tem escopo global, se for tem escopo apenas de função, isso pode gerar diversos problemas.
 
 console.log(idade) // Aqui já dá um erro, pois diferente de (var) const não tem escopo global e sim escopo de bloco.
-const idade = 18 //Const vem de (constante) ou seja, ela não pode ser atualizada nem declarada novamente
-idade = 20 // Isso aqui vai dar erro, pois const não pode ser atualziada.
+const idade = 18 //(const) vem de constante ou seja, ela não pode ser atualizada nem declarada novamente
+idade = 20 // Isso aqui vai dar erro, pois (const) não pode ser atualziada.
+
+let cidade = "Uberlandia" //(let) tem todas as caracterís de const, não tem escopo global e sim escopo de bloco. Mas diferente de const ele pode ser atualizada a qualquer momento.
+cidade = "São Paulo" // Isso aqui não dá erro.
+
